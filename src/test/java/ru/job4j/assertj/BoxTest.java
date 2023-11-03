9b77c9f4a077b4f27e9e3b9ed8424ec62344137c
INviZEo <lev19825@mail.ru>
2023-11-03 20:38:14 +0300

    3. Утверждения с примитивными типами [#504883 #457129]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -2,7 +2,8 @@ package ru.job4j.assertj;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.InstanceOfAssertFactories.BOOLEAN;
 
 class BoxTest {
     @Test
@@ -38,5 +39,37 @@ class BoxTest {
                 .isLessThan(9)
                 .isEven()
                 .isEqualTo(8);
+    }
+
+    @Test
+    void testExist() {
+        Box box = new Box(4, 10);
+        boolean result = box.isExist();
+        assertThat(result).isTrue();
+
+    }
+
+    @Test
+    void testNotExist() {
+        Box box = new Box(1, 10);
+        boolean result = box.isExist();
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void checkArea() {
+        Box box = new Box(4, 10);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(173d, withPrecision(0.6d))
+                .isLessThan(200);
+    }
+
+    @Test
+    void checkAreas() {
+        Box box = new Box(4, 6);
+        double result = box.getArea();
+        assertThat(result).isNotCloseTo(5d, withPrecision(0.6d))
+                .isLessThan(200)
+                .isGreaterThan(10);
     }
 }
