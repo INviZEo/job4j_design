17608ed0881e84b63cbefbcb9a295d9c7a4f7610
INviZEo <lev19825@mail.ru>
2023-11-03 21:46:38 +0300

    5. Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkToList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> array = simpleConvert.toList("one", "two");
+        assertThat(array).first().isEqualTo("one");
+    }
+
+    @Test
+    void checkToSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> array = simpleConvert.toSet("one", "two", "one");
+        assertThat(array).contains("two")
+                .contains("one")
+                .first()
+                .isEqualTo("one");
+    }
+
+    @Test
+    void checkToMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> array = simpleConvert.toMap( "1", "one", "garp", "stroka");
+        assertThat(array).hasSize(4)
+                .containsKeys("1")
+                .containsValue(3)
+                .containsEntry("garp", 2);
+    }
+}
