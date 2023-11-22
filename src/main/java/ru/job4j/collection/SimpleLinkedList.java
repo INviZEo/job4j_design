998b08bbbb85a3a860c0969c9f9ed94c1a6bf10c
INviZEo <lev19825@mail.ru>
2023-11-22 14:25:15 +0300

    2. Создать контейнер на базе связанного списка  [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,77 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+       Node<E> currentNode = head;
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+            Node<E> currentNode = head;
+            int i = 0;
+            while (index != i) {
+                i++;
+                currentNode = currentNode.next;
+            }
+        return currentNode.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            final int mCount = modCount;
+            private Node<E> nextNode = head;
+
+            @Override
+            public boolean hasNext() {
+                if (mCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return nextNode != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E item = nextNode.item;
+                nextNode = nextNode.next;
+                return item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
