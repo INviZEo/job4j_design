8021abe54287397e27e62e99788eed3b717987eb
INviZEo <lev19825@mail.ru>
2024-02-18 20:20:23 +0300

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ConsoleChat {
     private static final String OUT = "закончить";
@@ -26,47 +27,54 @@ public class ConsoleChat {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         boolean chatting = false;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         List<String> text = new ArrayList<>();
-        text = readPhrases();
         Random random = new Random();
 
         flag:
         while (!chatting) {
             System.out.print("Вы пишите: ");
             String userInput = scanner.nextLine();
-            saveLog(text);
             int randomIndex = random.nextInt(readPhrases().size());
-            System.out.println(randomIndex);
             String randomWord = readPhrases().get(randomIndex);
-            System.out.println("Бот отвечает: " + randomWord);
+            text.add("Вы пишите: " + userInput);
+            if (!STOP.equals(userInput)) {
+                System.out.println("Бот отвечает: " + randomWord);
+                text.add("Бот отвечает: " + randomWord);
+            }
             if (OUT.equals(userInput)) {
                 chatting = true;
                 System.out.println("Закончили из первого цикла");
+                text.add("Закончили из первого цикла");
             } else if (STOP.equals(userInput)) {
                 System.out.println("Бот теперь молчит");
+                text.add("Бот теперь молчит");
                 boolean chattingNoBot = false;
                 while (!chattingNoBot) {
                     System.out.print("Вы ведёте монолог: ");
                     String inputNoBot = scanner.nextLine();
+                    text.add("Вы ведёте монолог: " + inputNoBot);
                     if (CONTINUE.equals(inputNoBot)) {
                         System.out.println("Теперь бот отвечает");
+                        text.add("Теперь бот отвечает");
                         chattingNoBot = true;
                     } else if (OUT.equals(inputNoBot)) {
                         System.out.println("Закончили внутри второго цикла");
+                        text.add("Закончили внутри второго цикла");
                         break flag;
                     }
                 }
             }
         }
+        saveLog(text);
     }
 
 
     private List<String> readPhrases() {
         List<String> phrases = new ArrayList<>();
-        try (BufferedReader input = (new BufferedReader(
-                new FileReader("data/botanswers.txt", StandardCharsets.UTF_8)))) {
-            phrases.add(input.readLine());
+        try (BufferedReader input = (new BufferedReader(new FileReader("data/botanswers.txt", StandardCharsets.UTF_8)))) {
+            while (input.ready()) {
+                phrases.add(input.readLine());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -74,21 +82,15 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-        String filePath = "data/dialog.txt";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        Scanner scanner = new Scanner(System.in);
-        try {
-            FileWriter fileWriter = new FileWriter(filePath, StandardCharsets.UTF_8, true);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.println("[" + LocalDateTime.now().format(formatter) + "] Вы пишите: " + log.add(filePath));
-
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(path)))) {
+            log.forEach(printWriter::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        ConsoleChat consoleChat = new ConsoleChat("", "data/botanswers.txt");
+        ConsoleChat consoleChat = new ConsoleChat("data/dialog.txt", "data/botanswers.txt");
         consoleChat.run();
     }
 }
