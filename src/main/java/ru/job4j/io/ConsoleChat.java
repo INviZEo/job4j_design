b3754b0c4eefb0576a10d48df6d7604e9de8304f
INviZEo <lev19825@mail.ru>
2024-02-18 18:07:57 +0300

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -9,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class ConsoleChat {
     private static final String OUT = "закончить";
@@ -26,54 +27,50 @@ public class ConsoleChat {
         Scanner scanner = new Scanner(System.in);
         boolean chatting = false;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String filePath = "data/dialog.txt";
         List<String> text = new ArrayList<>();
+        text = readPhrases();
+        Random random = new Random();
 
-//        try {
-//            FileWriter fileWriter = new FileWriter(filePath, StandardCharsets.UTF_8, true);
-//            PrintWriter printWriter = new PrintWriter(fileWriter);
-
-            flag:
-            while (!chatting) {
-                System.out.print("Вы пишите: ");
-                String userInput = scanner.nextLine();
-                saveLog(text);
-//                readPhrases();
-//                printWriter.println("[" + LocalDateTime.now().format(formatter) + "] Вы пишите: " + userInput);
-                if (OUT.equals(userInput)) {
-                    chatting = true;
-                    System.out.println("Закончили из первого цикла");
-                } else if (STOP.equals(userInput)) {
-                    System.out.println("Бот теперь молчит");
-                    boolean chattingNoBot = false;
-                    while (!chattingNoBot) {
-                        System.out.print("Вы ведёте монолог: ");
-                        String inputNoBot = scanner.nextLine();
-//                        printWriter.println();
-                        if (CONTINUE.equals(inputNoBot)) {
-                            System.out.println("Теперь бот отвечает");
-                            chattingNoBot = true;
-                        } else if (OUT.equals(inputNoBot)) {
-                            System.out.println("Закончили внутри второго цикла");
-                            break flag;
-                        }
+        flag:
+        while (!chatting) {
+            System.out.print("Вы пишите: ");
+            String userInput = scanner.nextLine();
+            saveLog(text);
+            int randomIndex = random.nextInt(readPhrases().size());
+            System.out.println(randomIndex);
+            String randomWord = readPhrases().get(randomIndex);
+            System.out.println("Бот отвечает: " + randomWord);
+            if (OUT.equals(userInput)) {
+                chatting = true;
+                System.out.println("Закончили из первого цикла");
+            } else if (STOP.equals(userInput)) {
+                System.out.println("Бот теперь молчит");
+                boolean chattingNoBot = false;
+                while (!chattingNoBot) {
+                    System.out.print("Вы ведёте монолог: ");
+                    String inputNoBot = scanner.nextLine();
+                    if (CONTINUE.equals(inputNoBot)) {
+                        System.out.println("Теперь бот отвечает");
+                        chattingNoBot = true;
+                    } else if (OUT.equals(inputNoBot)) {
+                        System.out.println("Закончили внутри второго цикла");
+                        break flag;
                     }
                 }
             }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        }
     }
 
 
     private List<String> readPhrases() {
+        List<String> phrases = new ArrayList<>();
         try (BufferedReader input = (new BufferedReader(
                 new FileReader("data/botanswers.txt", StandardCharsets.UTF_8)))) {
-            input.lines().forEach(System.out::println);
+            phrases.add(input.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return phrases;
     }
 
     private void saveLog(List<String> log) {
@@ -83,7 +80,7 @@ public class ConsoleChat {
         try {
             FileWriter fileWriter = new FileWriter(filePath, StandardCharsets.UTF_8, true);
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.println("[" + LocalDateTime.now().format(formatter) + "] Вы пишите: " + log); //( Не пойму как сделать этот метод )
+            printWriter.println("[" + LocalDateTime.now().format(formatter) + "] Вы пишите: " + log.add(filePath));
 
         } catch (IOException e) {
             e.printStackTrace();
